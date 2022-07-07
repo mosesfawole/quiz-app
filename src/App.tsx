ffed59cc47bc978b0af1c68e618da3a5cc1d8a46
Moses Fawole <mosesfawole2001@gmail.com>
2022-07-07 19:20:33 +0100

    Setup for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import React from "react";
-function App() {
-  return <div className="App">Quiz</div>;
-}
+import QuestionCard from "./components/QuestionCard";
+const App = () => {
+  const startTrivia = async () => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const nextQuestion = () => {};
+  return (
+    <div>
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p className="Loading Questions ..."></p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
